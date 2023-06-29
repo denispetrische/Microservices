1dd9078c3d3a711859c18ae1d57e5150f559b9a8
0298287677@mail.ru <0298287677@mail.ru>
2023-06-29 17:22:15 +0300

    Created PlatformController and Docker

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            System.Console.WriteLine("--> Getting Platforms");
+            var platforms = _repo.GetAll();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platform = _repo.GetPlatformById(id);
+
+            if (platform == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<PlatformReadDto>(platform));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            var platformModel = _mapper.Map<Platform>(platform);
+            _repo.CreatePlatform(platformModel);
+            _repo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
+        }
+    }
+}

127531fc80635c07450aa4d6c70bd78ef371b8dc
0298287677@mail.ru <0298287677@mail.ru>
2023-07-12 12:28:35 +0300

    Created PlatformsController

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            System.Console.WriteLine("---> Inbounnd POST # CommandService");
+
+            return Ok("Inbound test of Platforms Controller");
+        }
+    }
+}
